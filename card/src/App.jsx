f91f87864c316ce0f74eaf4d781d6c3a245187a6
Freddy Lacey <fweddyshow@gmail.com>
2023-01-27 11:14:30 -0500

    made reusable

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 // import airbnb from './assets/airbnb.png'
+// import swim from './assets/katie.png'
 import './App.css'
 import Nav from "./components/nav"
 import Hero from "./components/hero"
@@ -8,11 +9,18 @@ import Card from "./components/card"
 export default function App() {
 
   return (
-    <>
+    <div>
     <Nav />
     <Hero />
-    <Card />
-    </>
+    <Card 
+    img="katie.png"
+    rating="5.0"
+    reviewCount={6}
+    country="USA"
+    title="Life Lessons with Katie Zaferes"
+    price={136}
+    />
+    </div>
   )
 
 }
--- card/src/components/card.jsx
+++ card/src/components/card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import star from "../assets/star.png"
-import swim from "../assets/bnb-img.png"
+import img from "../assets/katie.png"
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card--main">
-            <img src= {swim} className="card--swim" />
+            <img src={`../src/assets/${props.img}`} />
             <div>
                 <img src= {star} className='card--star' />
-                <span className="card--rating">5.0</span>
-                <span href="url" className="card--grey"> (6)</span>
-                <span className="card--grey"> • USA</span>
+                <span className="card--rating">{props.rating}</span>
+                <span href="url" className="card--grey"> ({props.reviewCount})</span>
+                <span className="card--grey"> • {props.country}</span>
             </div>
-            <p className="card--text">Life lessons with Katie Zaferes</p>
-            <p href="url" className="card--price"><span className="card--bold">From $136</span> / person</p>
+            <p className="card--text">{props.title}</p>
+            <p href="url" className="card--price"><span className="card--bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
